3f3dfc7000a6909abf4b606ebe42f803815f2fef
Vera <veraogamune06@gmail.com>
2026-03-05 11:47:32 +0100

    updared the Map embedded so it may point to the coorect coordinates

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 
 // Create transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
